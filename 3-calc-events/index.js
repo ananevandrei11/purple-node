d70fde07eebc0daf3db5ab455031ad6fc695d0a5
ananevandrei11 <ananevandrei11@gmail.com>
2026-06-02 22:50:38 +0200

    3-calc-events

--- 3-calc-events/index.js
+++ 3-calc-events/index.js
@@ -1,4 +1,9 @@
 const EventEmmiter = require("events");
+const { add } = require("./add");
+const { subtraction } = require("./subtraction");
+const { division } = require("./division");
+const { multiply } = require("./multiply");
+
 const myEventEmmiter = new EventEmmiter();
 
 let nodePath = process.argv[0];
@@ -7,48 +12,49 @@ let firstNum = process.argv[2];
 let secondNum = process.argv[3];
 let operation = process.argv[4];
 
-myEventEmmiter.addListener('add', (a,b) => {
-  console.log(a+b);
+myEventEmmiter.addListener('result', (res) => console.log(res));
+
+myEventEmmiter.addListener("add", (a, b) => {
+  const res = add(a,b);
+  myEventEmmiter.emit('result', res);
 });
 
-myEventEmmiter.addListener('subtraction', (a,b) => {
-  console.log(a-b);
+myEventEmmiter.addListener("subtraction", (a, b) => {
+  const res = subtraction(a,b);
+  myEventEmmiter.emit('result', res);
 });
 
-myEventEmmiter.addListener('multiply', (a,b) => {
-  console.log(a*b);
+myEventEmmiter.addListener("multiply", (a, b) => {
+  const res = multiply(a,b);
+  myEventEmmiter.emit('result', res);
 });
 
-myEventEmmiter.addListener('division', (a,b) => {
-  console.log(a/b);
+myEventEmmiter.addListener("division", (a, b) => {
+  const res = division(a,b);
+  myEventEmmiter.emit('result', res);
 });
-
-let res = 0;
-const a = Number(firstNum);
-const b = Number(secondNum);
 
 switch (operation) {
   case 'add':
   case '+': {
-    myEventEmmiter.emit('add', a, b);
+    myEventEmmiter.emit('add', firstNum, secondNum);
     break;
   }
   case 'multiply':
   case '*': {
-    myEventEmmiter.emit('multiply', a, b);
+    myEventEmmiter.emit('multiply', firstNum, secondNum);
     break;
   }
   case 'subtraction':
   case '-': {
-    myEventEmmiter.emit('subtraction', a, b);
+    myEventEmmiter.emit('subtraction', firstNum, secondNum);
     break;
   }
   case 'division':
   case '/': {
-    myEventEmmiter.emit('division', a, b);
+    myEventEmmiter.emit('division', firstNum, secondNum);
     break;
   }
   default:
-    console.log('Неизвестная операция:', operation);
+    myEventEmmiter.emit('result', `Неизвестная операция: ${operation}`);
 }
-
